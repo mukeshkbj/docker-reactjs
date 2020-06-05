555c95af6a8ad8287221b17a31c40b012e38fc53
Agrawal <MukeshKumar.Agrawal@in.pega.com>
2020-06-05 20:59:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Earn And Fun 
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Learn Earn And Fun/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
